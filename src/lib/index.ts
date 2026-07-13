dbac5b703745dca52f00e8c5a36a7af678eed5e5
Maddy Guthridge <hello@maddyguthridge.com>
2026-07-14 00:34:13 +1000

    refactor: define metrics from an array

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,16 +1,19 @@
 /** The metrics by which we judge our distros and our users. */
-export type Metric
+export const metrics = [
   /** How much does the person/distro like doing things themself. */
-  = 'diy'
-    /** How disorganised is the person/distro? */
-    | 'disorganised'
-    /** How much does the person/distro adopt new technologies. */
-    | 'earlyAdopter'
-    /** How in-touch with memes is the person/distro? */
-    | 'memetic'
-    /** How old is the person/distro (at heart)? How modern does it feel (tooling, etc) */
-    | 'unc'
-    /** Will they work consistently? Are they easy to accidentally break? */
-    | 'reliable'
-    /** How much does this person/distro judge others with different opinions */
-    | 'judgemental';
+  'diy',
+  /** How disorganised is the person/distro? */
+  'disorganised',
+  /** How much does the person/distro adopt new technologies. */
+  'earlyAdopter',
+  /** How in-touch with memes is the person/distro? */
+  'memetic',
+  /** How old is the person/distro (at heart)? How modern does it feel (tooling, etc) */
+  'unc',
+  /** Will they work consistently? Are they easy to accidentally break? */
+  'reliable',
+  /** How much does this person/distro judge others with different opinions */
+  'judgemental',
+] as const;
+
+export type Metric = typeof metrics[number];
