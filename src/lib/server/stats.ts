fba971766b5597619a01a03d7fc7eba88a7fa3f7
Maddy Guthridge <hello@maddyguthridge.com>
2026-07-15 03:50:49 +1000

    feat: support submitting results over API

--- src/lib/server/stats.ts
+++ src/lib/server/stats.ts
@@ -1,16 +1,22 @@
 import fs from 'node:fs/promises';
-import type { Personality } from '$lib/analysis';
 import path from 'node:path';
 import { DATA_PATH } from './consts';
 import { dev } from '$app/environment';
+import type { Results } from '../../routes/api/results/+server';
 
 const STATS_FILE = path.join(DATA_PATH, 'stats.json');
 
 export type ResponseStats = {
   /** Frequency of distro recommendations */
   distros: Record<string, number>,
-  /** Set of personalities of users */
-  personalities: Personality[],
+  /**
+   * Array of personalities of users.
+   *
+   * Note: not using the specific metrics here, as we cannot guarantee that we won't add more
+   * metrics over the course of the day, meaning not all metrics will exist in all submitted
+   * personalities.
+   */
+  personalities: Record<string, number>[],
   /**
    * Set of answers submitted by users.
    *
@@ -37,19 +43,35 @@ async function loadData() {
   }
 }
 
-export async function saveData(data: ResponseStats) {
+async function saveData(data: ResponseStats) {
   // mkdir -p, ignoring errors
   // NOTE: In production, this dir must already exist
   if (dev) {
     await fs.mkdir(DATA_PATH, { recursive: true }).catch(() => { });
   }
   // Now write the file
-  await fs.writeFile(STATS_FILE, JSON.stringify(data));
+  await fs.writeFile(STATS_FILE, JSON.stringify(data, undefined, 2));
 }
 
 let dataCache: ResponseStats | undefined = undefined;
 
+/** Get data in its current state, useful for calculating stats */
 export async function getData(): Promise<ResponseStats> {
   dataCache ??= await loadData();
   return dataCache;
+}
+
+export async function submitResults(results: Results) {
+  const data = await getData();
+
+  data.distros[results.distro] = (data.distros[results.distro] ?? 0) + 1;
+
+  data.personalities.push(results.personality);
+
+  for (const [q, a] of Object.entries(results.qandas)) {
+    data.qandas[q] ??= [];
+    data.qandas[q].push(a);
+  }
+
+  await saveData(data);
 }
--- src/routes/api/stats/+server.ts
+++
@@ -1,6 +0,0 @@
-import { json } from '@sveltejs/kit';
-
-export function POST() {
-  // TODO
-  return json({});
-}
