8df914961fdf57df4ac6d8f1396d2be5b40752c6
fabledsaint <35288585+fabledsaint@users.noreply.github.com>
2026-07-15 11:56:06 +1000

    feat: set up initial layout and header

---
+++ src/lib/components/site-header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import ModeToggle from './mode-toggle.svelte';
+</script>
+
+<header class="w-full bg-background">
+  <div class="mx-auto flex w-full items-center justify-between md:px-6 py-4">
+    <h1 class="text-xl font-bold">What Linux Distro Are You?</h1>
+    <ModeToggle />
+  </div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,16 @@
   import favicon from '$lib/assets/favicon.svg';
 
   import { ModeWatcher } from 'mode-watcher';
+  import SiteHeader from '$lib/components/site-header.svelte';
   const { children } = $props();
 </script>
 
 <ModeWatcher />
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+
+<div class="flex h-screen w-full mx-auto max-w-7xl items-center justify-center flex-col px-4">
+  <SiteHeader />
+  <main class="flex-1 flex-col flex">
+    {@render children()}
+  </main>
+</div>
