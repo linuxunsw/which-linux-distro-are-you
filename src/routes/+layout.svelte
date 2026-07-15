11058850cbdb96782cad7acf8101039838ac22b4
fabledsaint <35288585+fabledsaint@users.noreply.github.com>
2026-07-15 14:05:05 +1000

    feat: place quiz questions in a card, add a progress bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,9 +10,11 @@
 <ModeWatcher />
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<div class="flex h-screen w-full mx-auto max-w-7xl items-center justify-center flex-col px-4">
+<div
+  class="flex sm:p-x-2 md:p-x-4 h-screen w-full mx-auto max-w-7xl items-center justify-center flex-col px-4"
+>
   <SiteHeader />
-  <main class="flex-1 flex-col flex">
+  <main class="flex-1 flex-col flex w-full items-center">
     {@render children()}
   </main>
 </div>
