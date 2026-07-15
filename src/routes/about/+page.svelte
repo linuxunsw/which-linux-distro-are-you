a1b9f5d9ac0a96dcb818626e4233203a2cbb3d77
Maddy Guthridge <hello@maddyguthridge.com>
2026-07-15 14:32:16 +1000

    feat: add basic stats and about pages

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1>Which Linux Distro Are You?</h1>
+
+  <p><i>Made with &lt;3 by the UNSW Linux Society</i></p>
+
+  <ul>
+    <li>Backend: Maddy Guthridge</li>
+    <li>Frontend: FabledSaint</li>
+    <li>Writing: Maddy Guthridge, SolarNugget, Justin Chiam</li>
+  </ul>
+</main>
+
+<style>
+  :global(body) {
+    margin: 0;
+    height: 100vh;
+  }
+
+  main {
+    /* TODO: update once main UI code is merged */
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+
+</style>
---
+++ src/routes/stats/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import QrCode from '$lib/assets/qr-code.svg?raw';
+</script>
+
+<main>
+  <h1>Which Linux Distro Are You?</h1>
+
+  <div class="qr">
+    <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+    {@html QrCode}
+  </div>
+
+  <div class="stats">
+    <!-- TODO: Display statistics here -->
+  </div>
+</main>
+
+<style>
+  :global(body) {
+    margin: 0;
+    height: 100vh;
+  }
+
+  main {
+    /* TODO: update once main UI code is merged */
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+
+  .qr {
+    width: 30rem;
+    max-width: 90%;
+  }
+
+</style>
